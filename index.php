27d5b13bff4782c404216623181e054cc3a0ba91
John Shearer <john@ultranaut.com>
2017-10-05 17:05:35 -0400

    Tweaks

--- index.php
+++ index.php
@@ -13,15 +13,19 @@ Liquid::set('INCLUDE_PREFIX', '');
 
 $path = dirname(__FILE__) . DIRECTORY_SEPARATOR;
 
-$liquid = new Template($path . 'templates' . DIRECTORY_SEPARATOR);
-$liquid->parse(file_get_contents($path . 'templates' . DIRECTORY_SEPARATOR . 'daily.tpl'));
-
 // get feed content
 $client = new Client('http://api.hypster.com');
-$feed = $client->get('/posts/daily')->send()->json();
+$feed = $client->get('/posts/daily')
+               ->send()
+               ->json();
 
 // gotta throw in some stuff that would come from Maropost
 $feed['contact'] = array('email' => 'johns@junemedia.com');
 $feed['campaign'] = array('id' => time());
+
+
+// build template
+$liquid = new Template($path . 'templates' . DIRECTORY_SEPARATOR);
+$liquid->parse(file_get_contents($path . 'templates' . DIRECTORY_SEPARATOR . 'daily.tpl'));
 
 echo $liquid->render($feed);
